7a5d6bc50f41ede811056790a330379edf13b418
Garance Defrel <garance972@hotmail.com>
2023-10-28 00:36:58 +0200

    Navbar
    modification de la navbar

--- js/script.js
+++ js/script.js
@@ -1,18 +1,6 @@
-const toggleMenuBtn = document.querySelector("#menu-toggler");
-const toggleMenuImg = document.querySelector("#menu-toggler img");
-const mainNavlist = document.querySelector("#main-navlist");
-const menuLinks = document.querySelectorAll("#main-navlist a");
-
-[toggleMenuBtn, ...menuLinks].forEach((element) => element.addEventListener("click", toggleNav));
-
-function toggleNav() {
-  if (mainNavlist.classList.contains("hidden")) {
-    mainNavlist.classList.remove("hidden");
-    toggleMenuImg.setAttribute("src", "./assets/cross.svg");
-    toggleMenuBtn.setAttribute("aria-expanded", "true");
-  } else {
-    mainNavlist.classList.add("hidden");
-    toggleMenuImg.setAttribute("src", "./assets/menu.svg");
-    toggleMenuBtn.setAttribute("aria-expanded", "false");
-  }
+hamburger = document.querySelector('.hamburger');
+hamburger.onclick = function(){
+    navBar = document.querySelector('.navbar');
+    navBar.classList.toggle('active');
+   
 }
